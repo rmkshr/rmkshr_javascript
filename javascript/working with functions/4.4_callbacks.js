7434a4d7bf8fb2b92bfc7d14dfc47a60e1aaf99f
Ramkishore <>
2022-03-01 15:52:44 +0530

    Call back function changes

--- javascript/working with functions/4.4_callbacks.js
+++ javascript/working with functions/4.4_callbacks.js
@@ -1,2 +1,16 @@
 // Callbacks
+// Callback is a function that is passed as an argument to another function. 
 
+const greetUser = (userName, callBack) => { 
+    if(callBack){
+        console.log(callBack(userName));
+    } else { 
+        console.log(userName);
+    }
+    
+}
+
+const printUser = (userName) => console.log("Hello " + userName);
+
+
+greetUser("Ramkishore", printUser);
