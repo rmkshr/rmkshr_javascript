a54da0a48c5833148e6b5e437fa58e1fb9ff1a66
Ramkishore <>
2022-03-27 10:40:26 +0530

    practiced promise and mongo data update

--- test/test-update-flag.js
+++ test/test-update-flag.js
@@ -6,8 +6,13 @@ const {MongoClient} = require('mongodb');
 function establishConnection() {
     return new Promise((resolve, reject) => {
         const uri = "mongodb://localhost:27017/test?retryWrites=true&w=majority";
-        const client = new MongoClient(uri).connect();
-        resolve(client);
+        try {
+            const client = new MongoClient(uri).connect();
+            resolve(client);
+        } catch (exception) {
+            console.log(exception);
+            reject(exception);
+        }
     })
 }
 
@@ -20,8 +25,15 @@ function updateRegisteredFlag (dbClient, email) {
                 isRegistered : true
             },
         };
-        const result = dbClient.db("iamlegend").collection("user_seed").updateOne(filter, updateDoc, options);
-        resolve(result);
+        try {
+            const result = dbClient.db("iamlegend").collection("user_seed").updateOne(filter, updateDoc, options);
+            resolve(result);
+        } catch (exception) {
+            console.log(exception);
+            reject(exception.message);
+        }
+
+
     });
 }
 
